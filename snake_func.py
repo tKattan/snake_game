e4c185659844bf5f19205f6003a568882b97182f
Ectario <dimitritristangalaxy@gmail.com>
2020-05-09 13:42:59 +0200

    -Clear matrix added -In matrix object : columns and line changed -Snake updated with direction and speed added

--- snake_func.py
+++ snake_func.py
@@ -1,16 +1,24 @@
 import sys, pygame
 
-#13 lignes de 13 car on entoure par une couche set a -1 exprimant la bordure
+
+#11 lignes de 11 car on entoure par une couche set a -1 exprimant la bordure
         #0 : vide
         #1: snake body
         #2: head
         #3: apple
         #-1: border
+
+RIGHT= 0
+DOWN= 1
+LEFT= 2
+UP= 3
+
 class Matrix:
-    def __init__(self,x=11,y=11):   #Crée en fonction du nombre de colonne et de ligne que l'on veut une matrice
-                            #initialisée a 11 de baes mais peut être modulable : y = nb_colonne x = nb_ligne
-        self.colonne= y
-        self.ligne= x
+    def __init__(self,x=11,y=11):  
+         #Crée en fonction du nombre de ligne et de colonne que l'on veut une matrice
+         #             #initialisée a 11 de base mais peut être modulable : y = nb_ligne x = nb_colonne 
+        self.ligne= y
+        self.colonne= x
         self.matrix= []
         for i in range(y):
             self.matrix.append([])
@@ -24,31 +32,39 @@ class Matrix:
         return self.matrix
 
     def printMatrix(self):
-        for i in range(self.colonne):
+        for i in range(self.ligne):
             print("")
             print("[",end='')
-            for j in range(self.ligne):
+            for j in range(self.colonne):
                 print(" "+str(self.matrix[i][j]),end='')
             print("]",end='')
+    
+    def clearMatrix(self):
+        for i in range(self.ligne):
+            for j in range(self.colonne):
+                if i == 0 or i == self.ligne-1 or j == 0 or j == self.colonne-1 :
+                    self.matrix[i][j]=-1
+                else:
+                    self.matrix[i][j]=0
 
     def setMatrix(self,new_matrix):
-        for i in range(self.colonne):
-            for j in range(self.ligne):
+        for i in range(self.ligne):
+            for j in range(self.colonne):
                 self.matrix[i][j]=new_matrix[i][j]
 
     def setByPosMatrix(self,x,y,valeur):
         self.matrix[y][x]= valeur
 
     def getNumberOfLigne():
-        return self.colonne
-        
-    def getNumberOfColumns():
         return self.ligne
+
+    def getNumberOfColumns():
+        return self.colonne
 
 
 class Snake:
     #Initialisation de l'Objet snake, on insere sa taille au debut, la position de sa tete (position qui sera mettre dans une matrice donc ce n'est pas en pixel
-    # mais en emplacement)
+    # mais en case)
     def __init__(self,size_init=3, posHead = [5,5]):
         self.size = size_init
         self.posTail = []
@@ -61,13 +77,37 @@ class Snake:
             else:
                 self.posTail.append([posHead[0]-i,posHead[1]])
 
+
+    def updateSnake(self,direction=RIGHT,speed=1): #Les constantes de direction sont définies en haut du fichier
+        last_head = self.getHead()
+        last_tail = self.getTail()
+        new_tail = []
+        if direction == LEFT:
+            self.setHead([last_head[0]-speed,last_head[1]])
+        elif direction == RIGHT:
+            self.setHead([last_head[0]+speed,last_head[1]])
+        elif direction == UP:
+            self.setHead([last_head[0],last_head[1]-speed])
+        elif direction == DOWN:
+            self.setHead([last_head[0],last_head[1]+speed])
+
+        #la premiere partie de Tail va a la pos de l'ancienne position de head,
+        new_tail.append([last_head[0],last_head[1]])    
+        
+        # les autres morceaux de tail vont la place du morceau avant lui exeple : morceau 1 va a la place du morceau 0
+        for i in range(self.size-1):
+            new_tail.append(last_tail[i])
+
+        self.setTail(new_tail)
+
     def setHead(self,new_pos):
         self.posHead = new_pos
-    def setTail(self,new_pos):
-        self.posTail = new_pos
-
+    def setTail(self,new_tail):
+        self.posTail = new_tail
+    
     def getHead(self):
         return self.posHead
     def getTail(self):
         return self.posTail
+
 
